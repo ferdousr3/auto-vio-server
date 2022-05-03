95d4b7e7883f6e2b60bfa1922eaa6e15c2684cf7
ferdousr3 <ferdousr1992@gmail.com>
2022-05-03 15:33:01 +0600

    heroku add

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-<h1>
-  helloclear
-</h1>
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// root app
+app.get("/", (req, res) => {
+  res.send("Running Autovio Server");
+});
+
+app.listen(port, () => {
+  console.log(`Listing AutoVio - ${port}`);
+});
